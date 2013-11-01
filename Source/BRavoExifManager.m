4a814304010375702c93245288048014167d8d26
Hector Zarate <hecktorzr@gmail.com>
2013-11-01 19:51:48 +0100

    Capsuled method to flatten dictionaries inside dictionary

--- Source/BRavoExifManager.m
+++ Source/BRavoExifManager.m
@@ -10,6 +10,34 @@
 #import <ImageIO/ImageIO.h>
 
 
+@implementation NSDictionary (Utilities)
+
+- (NSDictionary *)flattenDictionaryObjects
+{
+    NSMutableDictionary *oneLevelDictionary = [NSMutableDictionary dictionary];
+    
+    for (id key in [self allKeys])
+    {
+        id objectForKey = self[key];
+        
+        if ([objectForKey isKindOfClass:[NSDictionary class]])
+        {
+            [oneLevelDictionary addEntriesFromDictionary:objectForKey];
+        }
+        else
+        {
+            [oneLevelDictionary setObject:objectForKey
+                                   forKey:key];
+        }
+    }
+    
+    return oneLevelDictionary;
+}
+
+@end
+
+
+
 @implementation BRavoExifManager
 
 - (NSDictionary *)metadataOfJPEGData:(NSData *)paramJPEGData
@@ -29,7 +57,6 @@
         CFDictionaryRef metadataReference = CGImageSourceCopyPropertiesAtIndex(imageSourceRef,
                                                                                0,
                                                                                NULL);
-        
         CFRelease(imageSourceRef);
         
         if (metadataReference)
@@ -38,24 +65,7 @@
             
             CFRelease(metadataReference);
             
-            NSMutableDictionary *oneLevelDictionary = [NSMutableDictionary dictionary];
-            
-            for (id key in [jpegMetadata allKeys])
-            {
-                id objectForKey = jpegMetadata[key];
-                
-                if ([objectForKey isKindOfClass:[NSDictionary class]])
-                {
-                    [oneLevelDictionary addEntriesFromDictionary:objectForKey];
-                }
-                else
-                {
-                    [oneLevelDictionary setObject:objectForKey
-                                           forKey:key];
-                }
-            }
-            
-            result = [NSDictionary dictionaryWithDictionary:oneLevelDictionary];
+            result = [jpegMetadata flattenDictionaryObjects];
         }
         
     }
@@ -64,3 +74,6 @@
 }
 
 @end
+
+
+
